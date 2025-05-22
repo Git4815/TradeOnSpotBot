9cb953d2f0a82a7809987e2bc2ee2e364f0314b8
Git4815 <141987544+Git4815@users.noreply.github.com>
2025-05-22 18:35:12 -0300

    Delete test.py
    Sensitive info breach

--- test.py
+++
@@ -1,6 +0,0 @@
-from mexc_sdk import Spot
-
-# Test MEXC SDK methods
-client = Spot(api_key="mx0vgl7XrPqWMYq4gM", api_secret="cca301c14247424c9ad7ff4ed1351af1")
-print(client.account_info())
-print(client.open_orders("USD1USDT"))
